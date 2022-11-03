a850a7be1097fdf7252337117ee446bee56d91d2
C0dingChris <Chris.Sanger@outlook.com>
2022-11-03 16:35:07 +0000

    Set up tests for 2 Katas(Codewars)

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 /* 
 👉 Write your kata here!
-*/
+
+*********************************************************
+                            *9 KYU*
+Multiply 2 numbers together return whether that number is even or odd
+
+To complete this task you will need to return an array with the multiplied number and a string of whether its odd or even
+
+your answer should return in the format [answer,"Odd"/"Even"]
+
+*********************************************************
 
 //👉 Write the function your CodeWarriors will start with below here:
+*/
+
+export function oddEvenMultiply(a,b){
+ 
+}
+
+/*
+*********************************************************
+                            *6 KYU*
+Given a word test whether it has every single vowel in the correct order
+
+a e i o u
+
+If it does return True otherwise return False 
+
+*********************************************************
+*/
+
+export function vowelOrder(word){
+
+}
+
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,45 @@
 //👉 Write your tests below here:
+import { test, expect } from "@jest/globals";
+import {oddEvenMultiply, vowelOrder} from "./main.js";
+
+
+
+//Tests for Challenge 1
+test("Check if calculated correctly", function(){
+    const actual = oddEvenMultiply(3,5);
+    const expected = [15,"Odd"];
+
+    expect(actual).toStrictEqual(expected);
+})
+
+test("Check if calculated correctly", function(){
+    const actual = oddEvenMultiply(4,5);
+    const expected = [20,"Even"];
+    expect(actual).toStrictEqual(expected);
+})
+
+test("Check if calculated correctly", function(){
+    const actual = oddEvenMultiply(-2,-3);
+    const expected = [6,"Even"];
+    expect(actual).toStrictEqual(expected);
+})
+
+//Tests for Challenge 2 
+test("Check if word contains all Vowels" , function(){
+    const actual = vowelOrder("facetious");
+    const expected = true;
+    expect(actual).toBe(expected);
+})
+
+test("Check if word contains all Vowels" , function(){
+    const actual = vowelOrder("happy");
+    const expected = false;
+    expect(actual).toBe(expected);
+})
+
+test("Check if word contains all Vowels in correct order" , function(){
+    const actual = vowelOrder("eutopia");
+    const expected = false;
+    expect(actual).toBe(expected);
+})
+
